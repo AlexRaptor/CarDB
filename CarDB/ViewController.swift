b7216dfa6cef756ca481ea8c2ade4d8c7173f958
Alexander Selivanov <alx_selivanov@icloud.com>
2019-02-14 15:38:03 +0700

    Initial Commit

---
+++ CarDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarDB
+//
+//  Created by Alexander Selivanov on 14/02/2019.
+//  Copyright © 2019 Alexander Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
