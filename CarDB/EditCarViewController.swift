b28011bfe1fbacde35fc601a5700801863d4f1fe
Alexander Selivanov <alx_selivanov@icloud.com>
2019-02-15 17:03:44 +0700

    update

---
+++ CarDB/EditCarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditCarViewController.swift
+//  CarDB
+//
+//  Created by Alexander Selivanov on 15/02/2019.
+//  Copyright © 2019 Alexander Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class EditCarViewController: UIViewController {
+
+    var car: Car?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
