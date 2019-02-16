db1a35b8c2e292d968e2ccbe4c883fabe4fdeb6f
Alexander Selivanov <alx.selivanov@gmail.com>
2019-02-16 18:14:07 +0700

    refactoring

--- CarDB/CarTableViewController.swift
+++ CarDB/CarTableViewController.swift
@@ -11,28 +11,40 @@ import CoreData
 
 class CarTableViewController: UITableViewController {
 
-    private var context: NSManagedObjectContext!
     private var cars = [Car]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
-
-        configureCoreData()
-
-        if !isDataExists() {
+        
+        // если в БД нет ни одного производителя, будем считать,
+        // что это первый запуск
+        // можно конечно хранить инфу про первый запуск в UserDefaults,
+        // но забьёи на это
+        do {
+            try DataService.shared.fetchManufacturers()
+        } catch {
+            fatalError("Error fetching manufacturers data: \(error.localizedDescription)")
+        }
+        
+        if DataService.shared.manufacturers.count == 0 {
 
             print("Data don't exists")
-
-            buildInitialDB(from: "Cars")
-
+            DataService.shared.buildInitialDB(from: "Cars")
+            
         } else {
             print("Data exists")
         }
 
-        fetchData()
-
+        do {
+            try DataService.shared.fetchAllData()
+        } catch {
+            fatalError("Error fetching data: \(error.localizedDescription)")
+        }
+        
+        cars = DataService.shared.cars
+        
         tableView.reloadData()
 
         print(cars)
@@ -73,140 +85,5 @@ extension CarTableViewController {
         print(car)
 
         tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
-// MARK: - Core Data
-
-extension CarTableViewController {
-
-    private func configureCoreData() {
-
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-
-        context = appDelegate.persistentContainer.viewContext
-    }
-
-    private func fetchData() {
-
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Car")
-
-        do {
-
-            guard let cars = try context.fetch(request) as? [Car] else { return }
-
-            self.cars = cars
-
-        } catch {
-            fatalError("Error fetching Cars: \(error.localizedDescription)")
-        }
-
-    }
-}
-
-// MARK: - Initial Data
-
-extension CarTableViewController {
-
-    private func isDataExists() -> Bool {
-
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Manufacturer")
-        //request.predicate = NSPredicate(format: "name = %@", "Ford")
-        request.returnsObjectsAsFaults = false
-        do {
-
-            let result = try context.fetch(request)
-
-            if result.count > 0 { return true }
-
-        } catch {
-            fatalError("Error requesting Data: \(error.localizedDescription)")
-        }
-
-        return false
-    }
-
-    private func buildInitialDB(from plistFileName: String) {
-
-        func readInitialData(from plistFileName: String) -> [[String: AnyObject]] {
-
-            if let path = Bundle.main.path(forResource: plistFileName, ofType: "plist") {
-
-                if let dataArray = NSArray(contentsOfFile: path) as? [[String: AnyObject]] {
-
-                    return dataArray
-
-                } else {
-                    fatalError("Can't read initial data into dictionary")
-                }
-            } else {
-                fatalError("Can't find file with initial data")
-            }
-
-            return []
-        }
-
-        func getDictionaryOfCarProperties<T: NSManagedObject>
-            (entity: T.Type, from dataArray: [[String: AnyObject]], key: String) -> [String: T] {
-
-            var dictionary = [String: T]()
-
-            for dataItem in dataArray {
-
-                if let itemName = dataItem[key] as? String {
-
-                    if dictionary[itemName] == nil {
-
-                        guard
-                            let entity = NSEntityDescription.entity(forEntityName: String(describing: T.self), in: context),
-                            let entityInstance = NSManagedObject(entity: entity, insertInto: context) as? T
-                            else { continue }
-
-                        entityInstance.setValue(itemName, forKey: "name")
-                        dictionary.updateValue(entityInstance, forKey: itemName)
-                    }
-                }
-            }
-
-            return dictionary
-        }
-
-        let data = readInitialData(from: plistFileName)
-
-        let manufacturers = getDictionaryOfCarProperties(entity: Manufacturer.self, from: data, key: "manufacturer")
-        let carClasses = getDictionaryOfCarProperties(entity: CarClass.self, from: data, key: "class")
-        let bodyTypes = getDictionaryOfCarProperties(entity: BodyType.self, from: data, key: "body")
-
-        for carData in data {
-
-            guard
-                let manufactuterName = carData["manufacturer"] as? String,
-                let model = carData["model"] as? String,
-                let year = carData["year"] as? Int,
-                let carClassName = carData["class"] as? String,
-                let bodyTypeName = carData["body"] as? String,
-                let manufacturer = manufacturers[manufactuterName],
-                let carClass = carClasses[carClassName],
-                let bodyType = bodyTypes[bodyTypeName],
-                let entity = NSEntityDescription.entity(forEntityName: "Car", in: context),
-                let car = NSManagedObject(entity: entity, insertInto: context) as? Car
-                else { continue }
-
-            car.setValue(manufacturer, forKey: "manufacturer")
-            car.setValue(model, forKey: "model")
-            car.setValue(year, forKey: "year")
-            car.setValue(carClass, forKey: "carclass")
-            car.setValue(bodyType, forKey: "bodytype")
-
-            manufacturer.addToCar(car)
-            carClass.addToCar(car)
-            bodyType.addToCar(car)
-        }
-
-        do {
-            try context.save()
-        } catch {
-            fatalError("Can't save initial data into context: \(error.localizedDescription)")
-        }
     }
 }
---
+++ CarDB/DataService.swift
@@ -0,0 +1,200 @@
+//
+//  DataService.swift
+//  CarDB
+//
+//  Created by Александр Селиванов on 16/02/2019.
+//  Copyright © 2019 Alexander Selivanov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DataService {
+    
+    static let shared = DataService()
+    
+    private let context: NSManagedObjectContext!
+    
+    // TODO: private(set)
+    var cars = [Car]()
+    var manufacturers = [Manufacturer]()
+    var carClasses = [CarClass]()
+    var bodyTypes = [BodyType]()
+    
+    private init() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+    }
+    
+    func saveData() throws {
+        do {
+            try context.save()
+        } catch {
+            throw error
+        }
+    }
+    
+    func fetchCars() throws {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Car")
+        
+        do {
+            
+            guard let cars = try context.fetch(request) as? [Car] else { return }
+            
+            self.cars = cars
+            
+        } catch {
+            throw error
+        }
+    }
+    
+    func fetchManufacturers() throws {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Manufacturer")
+        
+        do {
+            
+            guard let manufacturers = try context.fetch(request) as? [Manufacturer] else { return }
+            
+            self.manufacturers = manufacturers
+            
+        } catch {
+            throw error
+        }
+    }
+    
+    func fetchCarClasses() throws {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "CarClass")
+        
+        do {
+            
+            guard let carClasses = try context.fetch(request) as? [CarClass] else { return }
+            
+            self.carClasses = carClasses
+            
+        } catch {
+            throw error
+        }
+    }
+    
+    func fetchBodyTypes() throws {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BodyType")
+        
+        do {
+            
+            guard let bodyTypes = try context.fetch(request) as? [BodyType] else { return }
+            
+            self.bodyTypes = bodyTypes
+            
+        } catch {
+            throw error
+        }
+    }
+    
+    func fetchAllData() throws {
+        
+        do {
+            
+            try fetchCars()
+            try fetchManufacturers()
+            try fetchCarClasses()
+            try fetchBodyTypes()
+            
+        } catch {
+            throw error
+        }
+    }
+    
+}
+
+// MARK: - Initial Data
+
+extension DataService {
+    
+    func buildInitialDB(from plistFileName: String) {
+        
+        func readInitialData(from plistFileName: String) -> [[String: AnyObject]] {
+            
+            if let path = Bundle.main.path(forResource: plistFileName, ofType: "plist") {
+                
+                if let dataArray = NSArray(contentsOfFile: path) as? [[String: AnyObject]] {
+                    
+                    return dataArray
+                    
+                } else {
+                    fatalError("Can't read initial data into dictionary")
+                }
+            } else {
+                fatalError("Can't find file with initial data")
+            }
+            
+            return []
+        }
+        
+        func getDictionaryOfCarProperties<T: NSManagedObject>
+            (entity: T.Type, from dataArray: [[String: AnyObject]], key: String) -> [String: T] {
+            
+            var dictionary = [String: T]()
+            
+            for dataItem in dataArray {
+                
+                if let itemName = dataItem[key] as? String {
+                    
+                    if dictionary[itemName] == nil {
+                        
+                        guard
+                            let entity = NSEntityDescription.entity(forEntityName: String(describing: T.self), in: context),
+                            let entityInstance = NSManagedObject(entity: entity, insertInto: context) as? T
+                            else { continue }
+                        
+                        entityInstance.setValue(itemName, forKey: "name")
+                        dictionary.updateValue(entityInstance, forKey: itemName)
+                    }
+                }
+            }
+            
+            return dictionary
+        }
+        
+        let data = readInitialData(from: plistFileName)
+        
+        let manufacturers = getDictionaryOfCarProperties(entity: Manufacturer.self, from: data, key: "manufacturer")
+        let carClasses = getDictionaryOfCarProperties(entity: CarClass.self, from: data, key: "class")
+        let bodyTypes = getDictionaryOfCarProperties(entity: BodyType.self, from: data, key: "body")
+        
+        for carData in data {
+            
+            guard
+                let manufactuterName = carData["manufacturer"] as? String,
+                let model = carData["model"] as? String,
+                let year = carData["year"] as? Int,
+                let carClassName = carData["class"] as? String,
+                let bodyTypeName = carData["body"] as? String,
+                let manufacturer = manufacturers[manufactuterName],
+                let carClass = carClasses[carClassName],
+                let bodyType = bodyTypes[bodyTypeName],
+                let entity = NSEntityDescription.entity(forEntityName: "Car", in: context),
+                let car = NSManagedObject(entity: entity, insertInto: context) as? Car
+                else { continue }
+            
+            car.setValue(manufacturer, forKey: "manufacturer")
+            car.setValue(model, forKey: "model")
+            car.setValue(year, forKey: "year")
+            car.setValue(carClass, forKey: "carclass")
+            car.setValue(bodyType, forKey: "bodytype")
+            
+            manufacturer.addToCar(car)
+            carClass.addToCar(car)
+            bodyType.addToCar(car)
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            fatalError("Can't save initial data into context: \(error.localizedDescription)")
+        }
+    }
+}
